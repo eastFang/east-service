cf1ab10f31390d02dd4d51fbfb3e23fef58fbb05
eastFang <1139630194@qq.com>
2018-04-08 22:01:19 +0800

    fix: README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # service
 
+# backend
 koa2-router: https://chenshenhai.github.io/koa2-note/note/jsonp/info.html
+
+# fronend
+webpack react
--- frontend/app/component/button/index.jsx
+++ frontend/app/component/button/index.jsx
@@ -3,6 +3,6 @@ import "./index.scss"
 
 export default class extends React.Component {
   render() {
-    return <div class="btn-ui">按钮</div>
+    return <div class="btn-ui">按钮变动</div>
   }
 }
